935bf3e88066cb8522f49a3a03d369b37b1e2c63
Erifun <erfan.mahmoudi.2001@gmail.com>
2024-05-16 13:06:31 +0330

    updated views.py

--- blog/views.py
+++ blog/views.py
@@ -1,34 +1,16 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Article, category as categ
-#if you want to send HttpResponse or JsonResponse you should use the following commented code.
-#from django.http import HttpResponse , JsonResponse
 from django.http import Http404
 from django.core.paginator import Paginator
 
 from django.views.generic.list import ListView
 class ArticleList(ListView):
-    #model= Article
-    #context_object_name = "articles"
     template_name = "blog/home.html"
     queryset = Article.objects.filter(status="p").order_by('-publish')
     paginate_by = 4
 
-"""
-def home (request, page=1):
-    articles_list = Article.objects.filter(status="p").order_by('-publish')
-    paginator = Paginator(articles_list, 4)  # Show 25 contacts per page.
-    #page_number = request.GET.get("page")
-    articles = paginator.get_page(page)
-    context = {
-        # - means decending
-        "articles" : articles,
-    }
-    return render (request, "blog/home.html", context)
-"""
-
 def detail(request, slug):
     context = {
-        #Article.objects.get (slug=slug)
         "article" : get_object_or_404 (Article, slug=slug, status = "p"),
     }
     return render (request, "blog/detail.html", context)
